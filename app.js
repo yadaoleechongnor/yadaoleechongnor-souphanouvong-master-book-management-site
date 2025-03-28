b51e89a8814ed45bd02ff0d0e5a27abbf2c9fa42
Eagledeve <135152671+yadaoleechongnor@users.noreply.github.com>
2025-03-28 11:23:10 +0700

    Update app.js

--- app.js
+++ app.js
@@ -22,9 +22,9 @@ app.use(helmet({
 }));
 app.use(cors({
 origin: [
+    'https://vite-react-book-management-syste-production.up.railway.app'
     'http://localhost:5173', 
     'https://souphanouvonguniversity-book-management.onrender.com', 
-    'https://vite-react-book-management-syste-production.up.railway.app'
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true
